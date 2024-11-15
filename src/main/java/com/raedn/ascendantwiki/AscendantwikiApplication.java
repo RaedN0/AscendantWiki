b4000792836e9fcf20c3f8030fa7ed44b812161c
RaedN0 <marcel.schoerghuber@gmail.com>
2024-11-15 14:09:23 +0100

    init

---
+++ src/main/java/com/raedn/ascendantwiki/AscendantwikiApplication.java
@@ -0,0 +1,13 @@
+package com.raedn.ascendantwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscendantwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscendantwikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raedn/ascendantwiki/AscendantwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raedn.ascendantwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscendantwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
