dc47046a52e2c0c30fb57765a3f9e9e6dbd9865a
RaedN0 <marcel.schoerghuber@gmail.com>
2024-11-17 14:57:26 +0100

    add formlogin

--- src/main/java/com/raedn/ascendantwiki/config/SecurityConfiguration.java
+++ src/main/java/com/raedn/ascendantwiki/config/SecurityConfiguration.java
@@ -1,2 +1,34 @@
-package com.raedn.ascendantwiki.config;public class SecurityConfiguration {
+package com.raedn.ascendantwiki.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/login").permitAll()
+						.requestMatchers("/weapon").permitAll()
+						.anyRequest().authenticated()
+				)
+				.formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();  // Secure password hashing
+	}
 }
