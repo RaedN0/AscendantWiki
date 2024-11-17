e0a122b715f72fad8135dc7d9f92c1dde33c2040
RaedN0 <marcel.schoerghuber@gmail.com>
2024-11-17 16:11:32 +0100

    add endpoint to check authentication

--- src/main/java/com/raedn/ascendantwiki/controller/UserController.java
+++ src/main/java/com/raedn/ascendantwiki/controller/UserController.java
@@ -1,2 +1,19 @@
-package com.raedn.ascendantwiki.controller;public class UserController {
+package com.raedn.ascendantwiki.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/auth-status")
+	@ResponseBody
+	public String checkAuthStatus(Authentication authentication) {
+		if (authentication != null && authentication.isAuthenticated()) {
+			return "Authenticated as: " + authentication.getName();
+		}
+		return "Not authenticated";
+	}
 }
