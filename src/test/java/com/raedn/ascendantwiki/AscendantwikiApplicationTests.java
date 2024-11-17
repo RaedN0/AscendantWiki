5646859a3979196cc5710516e890d1f7c416d952
RaedN0 <marcel.schoerghuber@gmail.com>
2024-11-17 13:56:19 +0100

    add liquibase and create weapons table

--- src/test/java/com/raedn/ascendantwiki/AscendantwikiApplicationTests.java
+++ src/test/java/com/raedn/ascendantwiki/AscendantwikiApplicationTests.java
@@ -2,8 +2,10 @@ package com.raedn.ascendantwiki;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AscendantwikiApplicationTests {
 
 	@Test
