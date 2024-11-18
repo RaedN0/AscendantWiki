4d2125024206362e48bea7fa42409033a15df0fc
RaedN0 <marcel.schoerghuber@gmail.com>
2024-11-18 12:32:10 +0100

    get weapons from db

--- src/main/www/app/components/WeaponSelection.js
+++ src/main/www/app/components/WeaponSelection.js
@@ -1,12 +1,23 @@
-import { weapons } from "../entities/Weapons";
+"use client";
+
+import {useEffect, useState} from "react";
+import WeaponService from "@/app/services/WeaponService";
 
 export default function WeaponSelection({ selectedWeapon, setSelectedWeapon }) {
+    const [weapons, setWeapons] = useState([]);
+
+    useEffect(() => {
+        WeaponService.getWeapons().then(r => {
+            setWeapons(r);
+        })
+    }, [])
+
   return (
     <>
       <div className="section weapon-section">
         <h2>Choose Your Weapon</h2>
         <div className="weapon-buttons">
-          {weapons.map((weapon, index) => (
+          {weapons?.map((weapon, index) => (
             <button
               key={index}
               className={`weapon-button ${
