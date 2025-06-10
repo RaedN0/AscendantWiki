7696c30b4ac55a2fc2181ee2bdc1c8698c3bce0c
RaedN0 <marcel.schoerghuber@gmail.com>
2025-06-10 14:56:11 +0200

    fix navbar alignment

--- src/main/www/app/components/navbar/Navbar.js
+++ src/main/www/app/components/navbar/Navbar.js
@@ -1,69 +1,69 @@
 'use client';
 
-import React, {useContext} from 'react';
-import {AppBar, Box, Button, Toolbar, useMediaQuery, useTheme, Typography} from '@mui/material';
+import React, { useContext } from 'react';
+import { AppBar, Box, Button, Toolbar, useMediaQuery, useTheme, Typography } from '@mui/material';
 import Link from 'next/link';
 import MobileNavbar from "@/app/components/navbar/mobile";
 import DesktopNavbar from "@/app/components/navbar/desktop";
-import {RoleContext} from "@/app/RoleContext";
+import { RoleContext } from "@/app/RoleContext";
 import authenticationService from "@/app/services/AuthenticationService";
 import LoginButtons from "@/app/components/navbar/LoginButtons";
 
 const Navbar = () => {
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-    const {isAuthenticated, username} = useContext(RoleContext);
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+	const { isAuthenticated, username } = useContext(RoleContext);
 
-    const navItems = [
-        {text: 'Home', href: '/'},
-        // {text: 'Weapon Calculator', href: '/weapon-calculator'},
-        {text: 'Underdogs Leaderboard', href: '/leaderboard'},
-        {text: 'Events', href: '/events'},
-        {text: 'Sensitivity Converter', href: '/sensitivity'},
-    ];
+	const navItems = [
+		{ text: 'Home', href: '/' },
+		// {text: 'Weapon Calculator', href: '/weapon-calculator'},
+		{ text: 'Underdogs Leaderboard', href: '/leaderboard' },
+		{ text: 'Events', href: '/events' },
+		{ text: 'Sensitivity Converter', href: '/sensitivity' },
+	];
 
-    const loadoutItems = [
-        {text: 'Weapons', href: '/loadout/weapons'},
-        {text: 'Perks', href: '/loadout/perks'},
-        {text: 'Abilities', href: '/loadout/abilities'},
-    ];
+	const loadoutItems = [
+		{ text: 'Weapons', href: '/loadout/weapons' },
+		{ text: 'Perks', href: '/loadout/perks' },
+		{ text: 'Abilities', href: '/loadout/abilities' },
+	];
 
-    const buildsItems = [
-        {text: 'Loadouts', href: '/loadouts'},
-    ]
+	const buildsItems = [
+		{ text: 'Loadouts', href: '/loadouts' },
+	]
 
-    return (
-        <AppBar
-            position="sticky"
-            sx={{
-                backgroundColor: '#000000',
-                boxShadow: 'none',
-                borderBottom: `2px solid ${theme.palette.custom.main}`,
-                zIndex: 1000,
-            }}
-        >
-            <Toolbar
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    padding: '10px 20px',
-                }}
-            >
-                {isMobile ? (
-                    <Box sx={{display: 'flex', alignItems: 'center', justifyItems: 'center', gap: 2}}>
-                        <LoginButtons isAuthenticated={isAuthenticated} username={username}/>
-                        <MobileNavbar navItems={navItems} loadoutItems={loadoutItems} buildsItems={buildsItems}/>
-                    </Box>
-                ) : (
-                    <Box sx={{display: 'flex', alignItems: 'center', gap: 2}}>
-                        <DesktopNavbar navItems={navItems} loadoutItems={loadoutItems} buildsItems={buildsItems}/>
-                        <LoginButtons isAuthenticated={isAuthenticated} username={username}/>
-                    </Box>
-                )}
-            </Toolbar>
-        </AppBar>
-    );
+	return (
+		<AppBar
+			position="sticky"
+			sx={{
+				backgroundColor: '#000000',
+				boxShadow: 'none',
+				borderBottom: `2px solid ${theme.palette.custom.main}`,
+				zIndex: 1000,
+			}}
+		>
+			<Toolbar
+				sx={{
+					display: 'flex',
+					justifyContent: isMobile ? 'flex-end' : 'center',
+					alignItems: 'center',
+					padding: '10px 20px',
+				}}
+			>
+				{isMobile ? (
+					<Box sx={{ display: 'flex', alignItems: 'center', justifyItems: 'flex-end', gap: 2 }}>
+						<LoginButtons isAuthenticated={isAuthenticated} username={username} />
+						<MobileNavbar navItems={navItems} loadoutItems={loadoutItems} buildsItems={buildsItems} />
+					</Box>
+				) : (
+					<Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+						<DesktopNavbar navItems={navItems} loadoutItems={loadoutItems} buildsItems={buildsItems} />
+						<LoginButtons isAuthenticated={isAuthenticated} username={username} />
+					</Box>
+				)}
+			</Toolbar>
+		</AppBar>
+	);
 };
 
 export default Navbar;
